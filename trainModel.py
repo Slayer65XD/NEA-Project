3b19e68a36b7535c22b544653f51228213e9af0d
Adam Jimenez <adam.jimenez@gmail.com>
2024-12-14 18:31:55 +0000

    refactor

--- trainModel.py
+++ trainModel.py
@@ -6,33 +6,78 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import lightgbm as lgb
 from sklearn.metrics import accuracy_score
+import requests
+import json
+from datetime import datetime
 
-# Load data from the Premier League seasons 2014/15 to 2023/24
-filePath15 = 'data\PremierLeague15.csv'
-filePath16 = 'data\PremierLeague16.csv'
-filePath17 = 'data\PremierLeague17.csv'
-filePath18 = 'data\PremierLeague18.csv'
-filePath19 = 'data\PremierLeague19.csv'
-filePath20 = 'data\PremierLeague20.csv'
-filePath21 = 'data\PremierLeague21.csv'
-filePath22 = 'data\PremierLeague22.csv'
-filePath23 = 'data\PremierLeague23.csv'
-filePath24 = 'data\PremierLeague24.csv'
+import sqlite3
+connection = sqlite3.connect("football.db")
+cursor = connection.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS predictions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    round INTEGER NOT NULL,
+    home_team VARCHAR NOT NULL,
+    away_team VARCHAR NOT NULL,
+    result TINYINT NOT NULL
+)
+""")
 
-# 23/24 Data sourced from Kaggle and 14/15 to 22/23 Data sourced from ChatGPT formatted in the same way as the Kaggle csv file.
-data15 = pd.read_csv(filePath15)
-data16 = pd.read_csv(filePath16)
-data17 = pd.read_csv(filePath17)
-data18 = pd.read_csv(filePath18)
-data19 = pd.read_csv(filePath19)
-data20 = pd.read_csv(filePath20)
-data21 = pd.read_csv(filePath21)
-data22 = pd.read_csv(filePath22)
-data23 = pd.read_csv(filePath23)
-data24 = pd.read_csv(filePath24)
+apiKey = '9ca47c095388418992b1fa8b56265aba'
+currentYear = datetime.now().year
+
+csvData = []
+
+for i in range(10):
+
+    year = str(currentYear - i)
+
+    jsonPath = 'data/' + year + '.json'
+
+    print('season', year)
+    
+    csvPath = 'data/' + year + '.csv'
+    if not os.path.exists(csvPath):
+
+        if not os.path.exists(jsonPath):
+            print('downloading data')
+
+            # Load data from api
+            apiUrl = 'https://api.football-data.org/v4/competitions/PL/matches?season=2024' + year
+
+            try:
+                response = requests.get(
+                    apiUrl,
+                    headers={'X-Auth-Token': apiKey}
+                )
+            except requests.RequestException as e:
+                print(f'Error fetching data: {e}')
+
+            response.raise_for_status()
+
+            if response.text:
+                print('saving file')
+                f = open(jsonPath, "w")
+                f.write(response.text)
+                f.close()
+
+        with open(jsonPath) as f:
+            data = json.load(f)
+
+        csvData = 'round_number,home_team_name,away_team_name,home_team_goals,away_team_goals\n'
+        for match in data.get('matches', []):
+            print(match)
+            csvData += str(match['matchday']) + ',' + match['homeTeam']['shortName'] + ',' + match['awayTeam']['shortName'] + ',' + str(match['score']['fullTime']['home']) + ',' + str(match['score']['fullTime']['away']) + "\n"
+
+        # write to csv file
+        f = open(csvPath, "w")
+        f.write(csvData)
+        f.close()
+
+    csvData.append(pd.read_csv(csvPath))
 
 # Combine the datasets
-data = pd.concat([data15, data16, data17, data18, data19, data20, data21, data22, data23, data24], ignore_index=True)
+data = pd.concat(csvData, ignore_index=True)
 
 # Create target variable
 def determineResult(row):
@@ -49,8 +94,7 @@ data['result'] = data.apply(determineResult, axis=1)
 le = LabelEncoder()
 
 # Load 2024/25 fixtures
-fixturesPath = 'data\PremierLeagueFixtures25.csv'
-fixtures = pd.read_csv(fixturesPath)
+fixtures = pd.read_csv('data/2024.csv')
 
 # Combine historical teams with current fixtures to include all possible teams
 allTeams = pd.concat([
@@ -88,8 +132,8 @@ model.fit(xTrain, yTrain)
 yPred = model.predict(xTest)
 
 # Evaluate
-accuracy = accuracy_score(yTest, yPred)
-print(f'Model Accuracy: {accuracy:.2f}')
+confidence = accuracy_score(yTest, yPred)
+print(f'Model Confidence: {confidence:.2f}')
 
 # Encode the 2024/25 fixtures
 fixtures['homeTeamEncoded'] = le.transform(fixtures['home_team_name'])
@@ -98,10 +142,27 @@ fixtures['awayTeamEncoded'] = le.transform(fixtures['away_team_name'])
 # Predict the results for the 2024/25 season fixtures
 fixtures['predictedResult'] = model.predict(fixtures[['homeTeamEncoded', 'awayTeamEncoded']])
 
+# Empty table
+cursor.execute("""
+    DELETE FROM predictions
+""")
+
+# Save results to database
+for index, row in fixtures[['round_number', 'home_team_name', 'away_team_name', 'predictedResult']].iterrows():
+    #print(row)
+
+    cursor.execute("""
+    INSERT INTO predictions (round, home_team, away_team, result)
+    VALUES (?, ?, ?, ?)
+    """, (row['round_number'], row['home_team_name'], row['away_team_name'], row['predictedResult']))
+
 # Save predictions to a new CSV file
-outputPath = 'data/PredictedResults25.csv'
+outputPath = 'data/PredictedResults24.csv'
 fixtures[['round_number', 'home_team_name', 'away_team_name', 'predictedResult']].to_csv(outputPath, index=False)
 print('Predictions saved to:', outputPath)
 
 # Print a preview of the predictions
 print(fixtures.head())
+
+connection.commit()
+connection.close()
