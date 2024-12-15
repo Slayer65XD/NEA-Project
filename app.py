4007076588ca24e29a346af76143bcbf86ec2aa2
Adam Jimenez <adam.jimenez@gmail.com>
2024-12-15 12:35:42 +0000

    predictions in line with games played

--- app.py
+++ app.py
@@ -46,46 +46,9 @@ def homePage():
 
     with open(jsonFilePath) as f:
         data = json.load(f)
-
-    predictions = {}
-
-    # Load data from CSV
-    with open('data/PredictedResults24.csv', 'r') as csvFile:        
-        teams = {}
-
-        csvReader = csv.DictReader(csvFile)
-        for row in csvReader:
-            roundNumber = int(row['round_number'])
-
-            teams[row['home_team_name']] = teams.get(row['home_team_name'], 0) 
-            teams[row['away_team_name']] = teams.get(row['away_team_name'], 0) 
-
-            # Map numerical result to descriptive text
-            result = row['predictedResult'].strip()
-            if result == '1':
-                result = 'Home Win'
-                teams[row['home_team_name']] += 3
-            elif result == '0':
-                result = 'Draw'
-                teams[row['home_team_name']] += 1
-                teams[row['away_team_name']] += 1
-            elif result == '-1':
-                result = 'Away Win'
-                teams[row['away_team_name']] += 3                
-
-            predictions.setdefault(roundNumber, []).append({
-                'homeTeam': row['home_team_name'],
-                'awayTeam': row['away_team_name'],
-                'result': result
-            })
-
-    predictionsTable = dict(sorted(teams.items(), key=lambda item: item[1], reverse=True))
     
     teams = {}
     points = {}
-
-    complete = 0
-    correct = 0
 
     for match in data.get('matches', []):
         roundNumber = int(match['matchday'])
@@ -110,35 +73,82 @@ def homePage():
             result = 'Away Win'
             points[awayTeamName]['points'] += 3
         else:
-            result = 'None'        
+            result = 'None'      
 
-        results.setdefault(roundNumber, []);
-
-        prediction = predictions[roundNumber][len(results[roundNumber])]["result"]
-        
         if result != 'None':            
             points[homeTeamName]['played'] += 1
-            points[awayTeamName]['played'] += 1
+            points[awayTeamName]['played'] += 1  
 
-            complete = complete + 1
-
-            if result == prediction :
-                correct = correct + 1             
+        results.setdefault(roundNumber, []);          
 
         results[roundNumber].append({
             'homeTeam': homeTeamName,
             'awayTeam': awayTeamName,
             'result': result,
-            'prediction': prediction,
         })
 
     table = dict(sorted(points.items(), key=lambda item: item[1]['points'], reverse=True))
+
+    # gather predictions
+    predictions = {}
+    complete = 0
+    correct = 0
+
+    # Load data from CSV
+    with open('data/PredictedResults24.csv', 'r') as csvFile:        
+        predictedPoints = {}
+
+        csvReader = csv.DictReader(csvFile)
+        for row in csvReader:
+            roundNumber = int(row['round_number'])
+
+            predictedPoints[row['home_team_name']] = predictedPoints.get(row['home_team_name'], { 'points': 0, 'played': 0 }) 
+            predictedPoints[row['away_team_name']] = predictedPoints.get(row['away_team_name'], { 'points': 0, 'played': 0 })
+
+            # Map numerical result to descriptive text
+            result = row['predictedResult'].strip()
+            if result == '1':
+                result = 'Home Win'
+            elif result == '0':
+                result = 'Draw'
+            elif result == '-1':
+                result = 'Away Win'
+
+            predictions.setdefault(roundNumber, [])
+            actual = results[roundNumber][len(predictions[roundNumber])]['result']
+
+            predictions[roundNumber].append({
+                'homeTeam': row['home_team_name'],
+                'awayTeam': row['away_team_name'],
+                'result': result,
+                'actual' : actual
+            })            
+
+            # compare with actual result
+            if actual != 'None':
+                if result == 'Home Win':
+                    predictedPoints[row['home_team_name']]['points'] += 3
+                elif result == 'Draw':
+                    predictedPoints[row['home_team_name']]['points'] += 1
+                    predictedPoints[row['away_team_name']]['points'] += 1
+                elif result == 'Away Win':
+                    predictedPoints[row['away_team_name']]['points'] += 3
+                    
+                predictedPoints[row['home_team_name']]['played'] += 1
+                predictedPoints[row['away_team_name']]['played'] += 1  
+
+                complete = complete + 1
+
+                if result == actual :
+                    correct = correct + 1   
+
+    predictionsTable = dict(sorted(predictedPoints.items(), key=lambda item: item[1]['points'], reverse=True))
 
     accuracy = round((correct / complete) * 100)
 
     # Render results template with upcoming fixtures
     matchdays = sorted(results.keys())  # Sort matchdays in order
-    return render_template('index.html', results=results, matchdays=matchdays, table=table, predictionsTable=predictionsTable, complete=complete, correct=correct, accuracy=accuracy, teams=teams)
+    return render_template('index.html', predictions=predictions, matchdays=matchdays, table=table, predictionsTable=predictionsTable, complete=complete, correct=correct, accuracy=accuracy, teams=teams)
 
 if __name__ == '__main__':
     app.run(debug=True)
